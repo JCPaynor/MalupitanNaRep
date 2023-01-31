2cd70929eac9beaf4d756023e940cfa38d77f7dc
mj <mjm20mayuga@gmail.com>
2023-01-31 08:48:04 +0800

    Version with Functions

---
+++ SimpleCalculator.java
@@ -0,0 +1,21 @@
+public class SimpleCalculator{
+	public static void main(String [] xxx){
+		
+	}//end main
+	public static double add (int x, int y){
+		double output = x + y;
+		return output;
+	}// end add
+	public static double div (int x, int y){
+		double output = x / y;
+		return output;
+	}// end div
+	public static double mul (int x, int y){
+		double output = x * y;
+		return output;
+	}// end mul
+	public static double sub (int x, int y){
+		double output = x - y;
+		return output;
+	}// end sub
+}//end SimpleCalculator
